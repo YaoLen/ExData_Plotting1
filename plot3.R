da2006a9580ee9fc43de0044075d5160997aaae9
Sergey Zagorskiy <szagorskiy@gmail.com>
2015-11-06 17:41:29 +0400

    Added link to source loading data.

--- plot3.R
+++ plot3.R
@@ -1,4 +1,6 @@
 #The code to load data is placed to source file "get_and_explore.R"
+#Please check if required: https://github.com/YaoLen/ExData_Plotting1/blob/master/get_and_explore.R
+
 source("get_and_explore.R")
 
 #Fig3
